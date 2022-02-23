41acdf22566252c9282f33a6f3399cea8daa9c59
Abhishek Singh <saabhiahek@gmail.com>
2022-02-23 11:12:22 +0530

    json data update

--- pinterest-clone/server/src/index.js
+++ pinterest-clone/server/src/index.js
@@ -1,12 +1,13 @@
 const express= require('express');
 
 const {register,login}=require("./controllers/auth.controller.js")
+const productController = require("./controllers/product.controller.js")
 const app = express();
 
 
 
 app.use(express.json());
-
+app.use("/product",productController);
 app.post('/register',register);
 app.post('/login',login);
 // app.get('/data',data1)
