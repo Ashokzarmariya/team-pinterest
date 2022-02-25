5cd6487c18551962fd9a5854a1db0272794e6d6f
Nikita Patel <gniki0830@gmail.com>
2022-02-25 11:47:28 +0530

    main-page

--- pinterest-clone/client/src/components/Navbarm.jsx
+++ pinterest-clone/client/src/components/Navbarm.jsx
@@ -17,6 +17,51 @@ function closeForm1() {
 }
 
 export const Navbar = () => {
+  const signUp = async () => {
+    // e.preventDefault();
+
+    console.log("abc");
+    let emailS = document.getElementById("emailS").value;
+    let passS = document.getElementById("passS").value;
+    let user_data = {
+      email: emailS,
+      password: passS,
+    };
+    let data = JSON.stringify(user_data);
+
+    let res = await fetch("https://pinterest-backend8.herokuapp.com/register", {
+      method: "POST",
+      body: data,
+    });
+
+    res = await res.json();
+    console.log(res);
+    let token = res.token;
+  };
+
+  const logIn = async () => {
+    // e.preventDefault();
+
+    console.log("abc");
+    let emailL = document.getElementById("emailL").value;
+    let passL = document.getElementById("passL").value;
+    let user_data = {
+      email: emailL,
+      password: passL,
+    };
+    let data = JSON.stringify(user_data);
+    console.log(data);
+
+    let res = await fetch("https://pinterest-backend8.herokuapp.com/login", {
+      method: "POST",
+      body: data,
+    });
+
+    res = await res.json();
+    console.log(res);
+    let token = res.token;
+  };
+
   return (
     <div className="nav">
       <img className="pic" src="plogo.png"></img>
@@ -54,30 +99,32 @@ export const Navbar = () => {
 
       <div>
         <div className="form-popup" id="myForm">
-          <form action="/action_page.php" className="form-container">
+          <form className="form-container">
             <img className="logoimg1" src="p.png"></img>
-
             <h1 className="headinglogin1">Welcome back</h1>
 
             {/* <label for="email"><b>Email:</b></label> */}
-            <input
-              type="text"
-              placeholder="Enter Email"
-              name="email"
-              required
-            />
+            <input type="text" placeholder="Enter Email" id="emailL" required />
 
             {/* <label for="psw"><b>Password: </b></label> */}
             <input
               type="password"
               placeholder="Enter Password"
-              name="psw"
+              id="passL"
               required
             />
 
-            <button type="submit" className="btn">
+            <button
+              type="submit"
+              className="btn"
+              onClick={(e) => {
+                e.preventDefault();
+                logIn();
+              }}
+            >
               Login
             </button>
+
             <button
               type="button"
               className="btn cancel"
@@ -90,35 +137,35 @@ export const Navbar = () => {
 
             <hr className="hr"></hr>
 
-            <h1 className="loginc"> Not you? Log in with a different account</h1>
+            <h1 className="loginc">
+              {" "}
+              Not you? Log in with a different account
+            </h1>
             <h1 className="loginc1"> Need an account? Sign up now</h1>
           </form>
         </div>
 
         <div className="form-popup" id="myForm1">
-          <form action="/action_page.php" className="form-container">
+          <form className="form-container">
             <img className="logoimg1" src="p.png"></img>
-
             <h1 className="headingsignup">Welcome to Pinterest</h1>
             <h2 className="headingsignup1">Find new ideas to try</h2>
 
-            <input
-              type="text"
-              placeholder="Enter Email"
-              name="email"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="psw"
-              required
-            />
-            <input type="text" placeholder="Age" name="psw" required />
+            <input type="text" placeholder="Enter Email" id="emailS" />
+            <input type="password" placeholder="Enter Password" id="passS" />
+            <input type="text" placeholder="Age" name="age" />
 
-            <button type="submit" className="btn">
-              Login
+            <button
+              type="submit"
+              className="btn"
+              onClick={(e) => {
+                e.preventDefault();
+                signUp();
+              }}
+            >
+              Signup
             </button>
+
             <button
               type="button"
               className="btn cancel"
