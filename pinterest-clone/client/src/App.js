f48d7985c9c76b65223534e8a64c310531ab37eb
Raoji Patil <raojipatil22@gmail.com>
2022-02-27 11:33:50 +0530

    update

--- pinterest-clone/client/src/App.js
+++ pinterest-clone/client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import {Main} from "./components/main";
 
 function App() {
   return (
     <div className="App">
-      <h1>clone of pintet</h1>
+      <Main />
     </div>
   );
 }
